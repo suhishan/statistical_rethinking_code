f2621948a9ca7e7cfead91e51d4afc6d2790da8a
suhishan <suhishanbhandari@gmail.com>
2024-11-09 20:10:36 +0545

    lecture 9 fitting to data and simulating interventions.

--- lecture_09_berkeley.R
+++ lecture_09_berkeley.R
@@ -39,3 +39,84 @@ m2 <- ulam(
 precis(m2, depth = 3)
 inv_logit(coef(m2))
 
+
+# Let's use this in the real data for UCBerkeley --------------------------
+
+data("UCBadmit")
+d <- UCBadmit
+
+d <- as.list(d %>% mutate(
+  A = admit,
+  D = as.integer(dept),
+  G = ifelse(applicant.gender == "female",1,2),
+  N = applications
+) %>% select(A,D,G,N))
+
+# total causal effect.
+
+mG <- ulam(
+  alist(
+    A ~ dbinom(N, p),
+    logit(p) <- a[G],
+    a[G] ~ dnorm(0, 1)
+  ), data = d, chains = 4, cores = 4
+)
+
+# direct effect after including stratification for departments.
+
+mGD <- ulam(
+  alist(
+    A ~ dbinom(N, p),
+    logit(p) <- a[G,D],
+    matrix[G,D]:a ~ dnorm(0,1)
+  ), data = d, chains = 4, cores = 4
+)
+
+# Let's plot the constrasts for the direct effect.
+
+postG <- extract.samples(mG)
+View(postG$a)
+diffG <- inv_logit(postG$a[,1]) - inv_logit(postG$a[,2])
+dens(diffG, lwd = 1, col = 2, xlab = "Gender contrasts (in terms of probability")
+
+
+# let's the plot the posterior contrasts for the second model department wise.
+postGD  <- extract.samples(mGD)
+diffGD <- sapply(1:6, function(i)
+  inv_logit(postGD$a[,1,i]) - inv_logit(postGD$a[,2,i]))
+plot(NULL, xlim = c(-0.2, 0.3), ylim = c(0,25),
+     ylab = "Density",
+     xlab = "Gender Contrasts (probability)")
+for (i in 1:6) {
+  dens(diffGD[,i], add = TRUE, col = i+1, lwd = 2)
+}
+abline(v = 0, lty = 3)
+
+
+
+
+# Simulating an intervention ----------------------------------------------
+
+# Assuming the gender perception of the referees change.
+total_apps <- sum(d$N)
+
+apps_per_dept <- sapply(1:6, function(i)
+  sum(d$N[d$D == i]))
+
+# simulating as if all apps were from women
+# We have simulated the intervention where the data is in long form i.e.
+# in the form of Bernoulli trials and not binomial ones as used in the
+# model above. Not to worry though, the results will be the same. 
+p_G1 <- link(mGD, data = list(
+  D = rep(1:6, times = apps_per_dept),
+  G = rep(1, total_apps),
+  N = rep(1, total_apps)
+))
+
+p_G2 <- link(mGD, data = list(
+  D = rep(1:6, times = apps_per_dept),
+  G = rep(2, total_apps),
+  N = rep(1, total_apps)
+))
+
+dens(p_G1 - p_G2)
